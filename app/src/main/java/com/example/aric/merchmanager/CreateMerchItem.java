e1f1e76a3335d81572a79b9a1b71345aa2fac356
Aric Chan <gameroxer@hotmail.ca>
2019-05-08 22:08:24 -0400

    added some stuff

--- app/src/main/java/com/example/aric/merchmanager/CreateMerchItem.java
+++ app/src/main/java/com/example/aric/merchmanager/CreateMerchItem.java
@@ -1,7 +1,10 @@
 package com.example.aric.merchmanager;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class CreateMerchItem extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class CreateMerchItem extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_merch_item);
+    }
+
+    void CompleteMerchCreation() {
+        String name = ((EditText)findViewById(R.id.name)).getText().toString();
+        String set = ((EditText)findViewById(R.id.set)).getText().toString();
+        float price = Float.parseFloat(((EditText)findViewById(R.id.price)).getText().toString());
+        int quantity = Integer.parseInt(((EditText)findViewById(R.id.quantity)).getText().toString());
+
+        Intent merchDetails = new Intent();
+        merchDetails.putExtra("name", name);
+        merchDetails.putExtra("set", set);
+        merchDetails.putExtra("price", price);
+        merchDetails.putExtra("quantity", quantity);
     }
 }
--- app/src/main/java/com/example/aric/merchmanager/MerchStockManager.java
+++ app/src/main/java/com/example/aric/merchmanager/MerchStockManager.java
@@ -5,28 +5,46 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class MerchStockManager implements Serializable {
-    private HashMap<String, Integer> stockDictionary;
+    private HashMap<Integer, Integer> stockDictionary;
+    private HashMap<Integer, MerchItem> merchDictionary;
     private ArrayList<MerchSet> setDiscounts;
 
     public MerchStockManager() {
-        stockDictionary = new HashMap<String, Integer>();
+        stockDictionary = new HashMap<Integer, Integer>();
+        merchDictionary = new HashMap<Integer, MerchItem>();
         setDiscounts = new ArrayList<MerchSet>();
     }
 
-    public void AddStock(String nameOfItem, Integer amount) {
-        if (stockDictionary.containsKey(nameOfItem)) {
-            stockDictionary.put(nameOfItem, stockDictionary.get(nameOfItem) + amount);
+    public void AddStock(int itemId, Integer amount) {
+        if (stockDictionary.containsKey(itemId)) {
+            stockDictionary.put(itemId, stockDictionary.get(itemId) + amount);
         }
-        else stockDictionary.put(nameOfItem, amount);
+        else stockDictionary.put(itemId, amount);
     }
 
-    public void ResetStock(String nameOfItem) {
+    public void CreateItem(int itemId, String name, float price) {
+        MerchItem item = new MerchItem();
+        item.id = itemId;
+        item.name = name;
+        item.price = price;
+        merchDictionary.put(itemId, item);
+    }
+
+    public void ResetStock(int nameOfItem) {
         stockDictionary.put(nameOfItem, 0);
     }
 
-    public void RemoveStock(String nameOfItem, Integer amount) {
+    public void RemoveStock(int nameOfItem, Integer amount) {
         Integer amt = stockDictionary.get(nameOfItem) - amount;
         if (amt < 0) amt = 0;
         stockDictionary.put(nameOfItem, amt);
+    }
+
+    public int GenerateNewId() {
+        int retVal = merchDictionary.size();
+        while (merchDictionary.containsKey(retVal)) {
+            retVal++;
+        }
+        return retVal;
     }
 }
